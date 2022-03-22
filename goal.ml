42f88e9c377b237f9467c8d49adae252942d62a9
Jugurtha <jugurtha.1703@gmail.com>
2022-03-22 23:50:52 +0100

    print_goal with tprop list

--- goal.ml
+++ goal.ml
@@ -7,7 +7,7 @@
 
 type goal =
   ContextHoare of (string*tprop)list * hoare_triple
-|ContextTprop of (string*tprop)list *  tprop
+| ContextTprop of (string*tprop)list * tprop list
 ;;
 
 
@@ -19,7 +19,7 @@ let r = Prop(Const true);;
 let impl = Implied ((Or (p, q)), r);;
 
 let context1 = [("H", impl) ; ("H2", p)];;
-let conclusion1 = Or (p, q);;
+let conclusion1 = [Or (p, q)];;
 let goal1 = ContextTprop ( context1, conclusion1 );;
 
 
@@ -29,7 +29,7 @@ let goal2 = ContextHoare ( [],
                                       InfEqual(Var "x", Const 0),
                                       Affect("x", Minus(Const 0, Var "x")),
                                       Skip),
-                                  Equal(Const 3, Var "x")              
+                                  Equal( Var "x", Const 3)              
                              )
               )
 ;;
@@ -38,19 +38,26 @@ let goal2 = ContextHoare ( [],
 (* Question 3 : *)
 
 let rec context_to_string context =
-    match context with
-    | [] -> ""
-    | (id, prop)::rl -> id ^ " : " ^ (prop_to_string prop) ^ "\n" ^ (print_context rl)
+  match context with
+  | [] -> ""
+  | (id, prop)::rl -> id ^ " : " ^ (prop_to_string prop) ^ "\n" ^ (context_to_string rl)
 ;;
+
+let rec conclusion_to_string conclusion =
+  match conclusion with
+  | [] -> ""
+  | tprop::tail -> "=============================\n" ^ (prop_to_string tprop) ^ (conclusion_to_string tail)
+;;
+
 
 print_string (context_to_string context1);;
 
 let rec print_goal goal =
   match goal with
   | ContextHoare (context, conclusion) ->
-     print_endline ( (context_to_string context1) ^ "======================\n" ^ ("prop_to_string conclusion"))
+     print_endline ( (context_to_string context1) ^ "======================\n" ^ (hoare_triple_to_string conclusion))
   | ContextTprop (context, conclusion) ->
-     print_endline ( (context_to_string context1) ^ "======================\n" ^ (prop_to_string conclusion))      
+     print_endline ( (context_to_string context1) ^ (conclusion_to_string conclusion) )      
 ;;
 
 print_goal goal1;;
